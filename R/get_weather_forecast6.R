b367f622ce1d7524774e33db227b4ad84f0ab81b
LucasLD56 <lucas.ledrevo@gmail.com>
2024-03-04 16:28:24 +0100

    Premier commit : Ajout du code R

---
+++ R/get_weather_forecast6.R
@@ -0,0 +1,8 @@
+# Assurez-vous que la librairie usethis est chargée
+library(usethis)
+
+# Créer un nouveau script R nommé get_weather_forecast
+use_r("get_weather_forecast")
+
+library(olympicsWeather)
+
